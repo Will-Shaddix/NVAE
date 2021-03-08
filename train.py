6038eaca673d63daeaefb23cbb54ac2c75d4d177
William Shaddix <billy.shaddix@gmail.com>
2021-03-08 10:55:59 -0800

    added to print

--- train.py
+++ train.py
@@ -276,7 +276,7 @@ def init_processes(rank, size, fn, args):
     """ Initialize the distributed environment. """
     os.environ['MASTER_ADDR'] = args.master_address
     os.environ['MASTER_PORT'] = '6020'
-    print(torch.cuda.getDeviceCount())
+    print("Get Device:", torch.cuda.getDeviceCount())
 
     torch.cuda.set_device(args.local_rank)
     dist.init_process_group(backend='nccl', init_method='env://', rank=rank, world_size=size)
