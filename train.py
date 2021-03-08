325eecd11aebc186c57281529cae25f23d3912d6
William Shaddix <billy.shaddix@gmail.com>
2021-03-08 11:12:54 -0800

    added a new print

--- train.py
+++ train.py
@@ -277,6 +277,7 @@ def init_processes(rank, size, fn, args):
     os.environ['MASTER_ADDR'] = args.master_address
     os.environ['MASTER_PORT'] = '6020'
     print("Device Count:", torch.cuda.device_count())
+    print("Current Device:", torch.cuda.current_device())
 
     torch.cuda.set_device(args.local_rank)
     dist.init_process_group(backend='nccl', init_method='env://', rank=rank, world_size=size)
